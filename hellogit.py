ce1e3e9fa472316bd7324246ee6bae9d3b5a7d58
Manuel Florez <mfelipefl@gmail.com>
2024-01-03 20:28:40 -0500

    realice clonacion de repositorio

---
+++ hellogit.py
@@ -0,0 +1,5 @@
+print("Hello Git New 2!!!!")
+
+##recordemos que podemos realizar varias modificaciones al archivo
+
+print("hello Git New 3!!!!") 
---
+++ hellogit2.py
@@ -0,0 +1,5 @@
+a = 2
+b = 5
+c = a + b
+print ("Esto es el resultado de la suma", c)
+
